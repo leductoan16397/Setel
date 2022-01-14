a15e4f8a62be3b913286ebe3b9bca5536d8860af
Toan Le <leductoan16397@gmail.com>
2022-01-15 00:15:17 +0700

    add new exception factory

--- server/order/src/core/exception/exceptionFactory.ts
+++ server/order/src/core/exception/exceptionFactory.ts
@@ -2,7 +2,7 @@ import { ValidationError } from 'class-validator';
 import { ExceptionError } from './interface/exceptionError.interface';
 import { BadRequestException } from '@nestjs/common';
 
-export const ExceptionFactory = (errors: ValidationError[]) => {
+export const ExceptionFactory2 = (errors: ValidationError[]) => {
   const mewErrors: ExceptionError[] = errors.map((error) => {
     return generateValidation(error);
   });
@@ -20,4 +20,22 @@ const generateValidation = (error: ValidationError): ExceptionError => {
     err.children = error.children.map(generateValidation);
   }
   return err;
+};
+export const ExceptionFactory = (errors: ValidationError[]) => {
+  const mewErrors: ExceptionError2 = {};
+  errors.forEach((error) => {
+    generateValidation2(error, mewErrors);
+  });
+  throw new BadRequestException({ message: mewErrors });
+};
+export interface ExceptionError2 {
+  [key: string]: string[]
+}
+const generateValidation2 = (error: ValidationError, mewErrors: ExceptionError2, parent?: string) => {
+  if (error.constraints) {
+    mewErrors[parent ? `${parent}.${error.property}` : error.property] = Object.values(error.constraints);
+  }
+  if (error.children.length > 0) {
+    error.children.forEach(item => generateValidation2(item, mewErrors, parent ? `${parent}.${error.property}` : error.property));
+  }
 };
